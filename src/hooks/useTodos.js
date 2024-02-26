f8941447b7dbabc99cecb27e4beab58402c2d1d5
Husenov Hasan <husenovhasan5577@gmail.com>
2024-02-26 17:46:13 +0500

    constants query keys

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { TodoService } from "../services";
+import { queryKeys } from "../shared";
 
 const { getTodos, getTodosById } = TodoService;
 export const useTodos = () => {
   return useQuery({
-    queryKey: ["todos"],
+    queryKey: queryKeys.Todos,
     queryFn: getTodos,
     // enabled: true,     // мы можем показать данные при определенном условии
     select: ({ data }) => data,
@@ -15,7 +16,7 @@ const todoById = 1;
 
 export const useTodosById = () => {
   return useQuery({
-    queryKey: ["todos", todoById],
+    queryKey: [...queryKeys.Todos, todoById],
     queryFn: () => {
       return getTodosById(todoById);
     },
